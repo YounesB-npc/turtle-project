0834d15087751dd990c51dca9d884574db41a960
Younes <younes.bakir@mcvts.org>
2025-09-18 10:13:18 -0400

    Update turt.py

--- turt.py
+++ turt.py
@@ -3,11 +3,13 @@
 #Turtle Project: MSPaint Ripoff
 #Extra: Color shifting line
 
+#import required modules
 import turtle
 import colorsys
 
 hue = 0
 
+#This function will set up our color shift
 def set_turtle_color():
     global hue
 
@@ -18,14 +20,16 @@ def set_turtle_color():
     t.pencolor(rgb)
 
     #Increment hue 
-    hue += 0.03
+    hue += 0.01
     if hue >= 1:
             hue = 0
 
+#Set up the turtle, canvas, and adjust background color
 t = turtle.Turtle()
 s = turtle.Screen()
-turtle.bgcolor('lightgray')
+turtle.bgcolor('black')
 
+#All these functions set up movement directions and enable the color shift
 def up():
     set_turtle_color()
     t.forward(50)
@@ -35,13 +39,30 @@ def down():
     t.backward(50)
 
 def left():
-    set_turtle_color()
     t.left(45)
 
 def right():
-    set_turtle_color()
     t.right(45)
 
+#These functions allow the pensize to be adjusted
+def penIncrease():
+    current_size = t.pensize()
+    new_size = t.pensize(current_size + 1)
+    t.pensize(new_size)
+
+def penDecrease():
+    current_size = t.pensize()
+    new_size = t.pensize(current_size - 1)
+    t.pensize(new_size)
+
+#Controls pensize adjustments
+def pensize():
+    pass
+s.listen()
+s.onkey(penIncrease, '1') #Increase pen size if 1 is pressed
+s.onkey(penDecrease, '2') #Increase pen size if 2 is pressed
+
+#Controls movement 
 def movement():
     pass
 s.listen()
@@ -51,4 +72,3 @@ s.onkey(left, 'a') #Rotate if a is pressed
 s.onkey(right, 'd') #Rotate if d is pressed
 
 turtle.done()
-
